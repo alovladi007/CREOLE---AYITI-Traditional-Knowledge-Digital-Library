d7bbe197dae23e0ca0a8ae322e416fa51bd2e88d
alovladi007 <83262803+alovladi007@users.noreply.github.com>
2025-10-05 20:32:01 -0400

    fix: Update json2csv package to @json2csv/plainjs v7

--- backend/src/records/records-bulk.service.ts
+++ backend/src/records/records-bulk.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { RecordEntity } from './entities/record.entity';
-import { Parser } from 'json2csv';
+import { Parser } from '@json2csv/plainjs';
 
 @Injectable()
 export class RecordsBulkService {
